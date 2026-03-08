7ffd0b422ea0df119e93d123f3d6ffcba2df0558
jessica andrade <malupessoal.baby@gmail.com>
2026-03-08 20:18:33 -0300

    vercel requests

--- baby-list/frontend/babyList/src/lib/supabase.ts
+++ baby-list/frontend/babyList/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = 'https://bcnwgcwhwqlveytaalik.supabase.co'
-const supabaseKey = 'sb_publishable_lSafo5XuMVB7p5qMf7wY-g_c_zp9A-z'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
